b4fa7b96acf3f48cc6ceb35378e30dae057a6dfa
Shomeek <54298697+Shomeek@users.noreply.github.com>
2019-10-20 11:47:25 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,41 @@
+
+const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+var difference = '';
+
+// a and b are javascript Date objects
+function dateDiffInDays(a, b) {
+  // Discard the time and time-zone information.
+  const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+  const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+  return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+}
+difference = dateDiffInDays(a, b);
+
+document.getElementById('datecalc').innerHTML = difference;
+
+document.getElementById('datecalcs').innerHTML = difference;
+
+
+//===================================manual image slider======================================
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
+
+
